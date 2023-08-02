b90e16c90f2a50be59563ea7ba600ad7666d4b6b
Vishnupriya <vishnupriya.t7@gmail.com>
2023-08-02 16:27:56 +0530

    heder-menu-modified

--- portfolio-details.php
+++ portfolio-details.php
@@ -51,7 +51,7 @@
         <div class="d-flex justify-content-between align-items-center">
           <h2>Portfolio Details</h2>
           <ol>
-            <li><a href="index.html">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="portfolio.html">Portfolio</a></li>
             <li>Portfolio Details</li>
           </ol>
