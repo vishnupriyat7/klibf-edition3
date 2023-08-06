14f66be80d14b8ff1ef3e84ed00ec3d466931595
Vishnupriya <vishnupriya.t7@gmail.com>
2023-08-06 19:12:54 +0530

    new-login-registration-integrated

--- menu.php
+++ menu.php
@@ -60,7 +60,7 @@
             </ul>
           </li> -->
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="nav-link scrollto" href="register_login.php">Login</a></li>
+          <li><a class="nav-link scrollto" href="login.php">Login</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav>
