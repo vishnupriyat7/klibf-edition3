2bc5e4a4069f9cc0a70fddcc983a57f33500d6a0
parvathi <parvathimpillai@gmail.com>
2023-08-19 13:47:35 +0530

    save data of book discussion proposal and retrieve data

--- dashboard/publisher_bookdiscussion.php
+++ dashboard/publisher_bookdiscussion.php
@@ -88,15 +88,16 @@ $user_id = $user['id'];
                                     $status = "NOTOK";
                                 }
                             } else {
-                                if (!$logo) {
+                                if (!$disc_book_cover) {
                                     $msg = 'Please select an image file to upload.';
                                     $status = "NOTOK";
-                                }                                
+                                }
                             }
-                            if($time_slot == '0') {
+                            if ($time_slot == '0') {
                                 $msg = $msg . "Please select proposed time slot.<BR>";
                                 $status = "NOTOK";
-                            } if($evnt_day == '0') {
+                            }
+                            if ($evnt_day == '0') {
                                 $msg = $msg . "Please select proposed date.<BR>";
                                 $status = "NOTOK";
                             }
@@ -106,68 +107,15 @@ $user_id = $user['id'];
                                     $msg . "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                                                </div>"; //printing error if found in validation
                             } else {
-                                // if ($user_id) {
-                                   
-                                    // $query = "UPDATE users_profile SET org_name = '$comp_name', estb_year = '$estb_year', reg_no = '$reg_no', gst_no = '$gst_no', book_lang = '$book_lang', title_no = '$title_no', org_nature = '$org_nature', mgr_house_name = '$mgr_pub_hse', head_org_name = '$head_name', head_org_addr = '$head_addr', head_org_mobile = '$head_mobile', head_org_email = '$head_email', head_org_website = '$head_site', cntct_prsn_name = '$prsn_name', cntct_prsn_addr = '$prsn_addr', cntct_prsn_mobile = '$prsn_mobile', cntct_prsn_email = '$prsn_email', cntct_prsn_watsapp = '$whatsapp', status = 'E', updated_at = '$date', fascia = '$fascia', remarks = '$remark', logo = '$imgContent' WHERE user_id = '$user_id'";
-                                    
-                                // } else {
-                                    $query = "INSERT INTO evnt_propsl_bkdscn (user_id, subject, moderator, participant1, participant2, participant3, participant4, date_prefering, time_prefering, cntct_name, cntct_phno, cntct_mail, updated_at, remarks, submitted) VALUES ('$user_id', '$disc_sub', '$modrtr', '$prtcpnt1', '$prtcpnt2', '$prtcpnt3', '$prtcpnt4', '$evnt_day', '$time_slot', '$cntct_name', '$cntct_phno', '$cntct_mail', '$date', '$disc_remark', '0')";
-                                // }
+                                $query = "INSERT INTO evnt_propsl_bkdscn (user_id, subject, moderator, participant1, participant2, participant3, participant4, date_prefering, time_prefering, cntct_name, cntct_phno, cntct_mail, updated_at, remarks, status, book_cover) VALUES ('$user_id', '$disc_sub', '$modrtr', '$prtcpnt1', '$prtcpnt2', '$prtcpnt3', '$prtcpnt4', '$evnt_day', '$time_slot', '$cntct_name', '$cntct_phno', '$cntct_mail', '$date', '$disc_remark', 'E', '$imgContent')";
                                 $result = mysqli_query($con, $query);
                                 if ($result) {
                                     $errormsg = "
                               <div class='alert alert-success alert-dismissible alert-outline fade show'>
                                                 Your Profile Details is Successfully Saved. Proceed with stall(s) booking.
                                                 <button type='button' class='btn-close' data-dismiss='alert' aria-label='Close'></button>
-                                                </div>
-                                              
-                               ";
-                                    // $sql1 = "SELECT * FROM users_profile WHERE user_id = ?;";
-                                    // $stmt1 = $con->prepare($sql1);
-                                    // $stmt1->bind_param("s", $user_id);
-                                    // $stmt1->execute();
-                                    // $result1 = $stmt1->get_result();
-                                    // $user_profile = $result1->fetch_assoc();
-                                    // $comp_name = $user_profile['org_name'];
-                                    // $estb_year = $user_profile['estb_year'];
-                                    // $reg_no = $user_profile['reg_no'];
-                                    // $gst_no = $user_profile['gst_no'];
-                                    // $book_lang = $user_profile['book_lang'];
-                                    // $title_no = $user_profile['title_no'];
-                                    // $org_nature = $user_profile['org_nature'];
-                                    // $mgr_pub_hse = $user_profile['mgr_house_name'];
-                                    // $head_name = $user_profile['head_org_name'];
-                                    // $head_addr = $user_profile['head_org_addr'];
-                                    // $head_mobile = $user_profile['head_org_mobile'];
-                                    // $head_email = $user_profile['head_org_email'];
-                                    // $head_site = $user_profile['head_org_website'];
-                                    // $prsn_name = $user_profile['cntct_prsn_name'];
-                                    // $prsn_addr = $user_profile['cntct_prsn_addr'];
-                                    // $prsn_mobile = $user_profile['cntct_prsn_mobile'];
-                                    // $prsn_email = $user_profile['cntct_prsn_email'];
-                                    // $whatsapp = $user_profile['cntct_prsn_watsapp'];
-                                    // $fascia = $user_profile['fascia'];
-                                    // $remark = $user_profile['remarks'];
-                                    // $logo = base64_encode($user_profile['logo']);
-                                    // if ($org_nature == 'A') {
-                                    //     $select0 = '';
-                                    //     $selecta = 'selected';
-                                    //     $selectp = '';
-                                    // } else if ($org_nature == 'P') {
-                                    //     $select0 = '';
-                                    //     $selecta = '';
-                                    //     $selectp = 'selected';
-                                    // } else {
-                                    //     $select0 = 'selected';
-                                    //     $selecta = '';
-                                    //     $selectp = '';
-                                    // }
-                                    // if (!$logo) {
-                                    //     $hide = "";
-                                    // } else {
-                                    //     $hide = "hidden";
-                                    // }
-                                }  else {
+                                                </div>";
+                                } else {
                                     $errormsg = "
                                     <div class='alert alert-danger alert-dismissible alert-outline fade show'>
                                                Some Technical Glitch Is There. Please Try Again Later Or Ask Admin For Help.
@@ -194,36 +142,36 @@ $user_id = $user['id'];
                                             </div> -->
                                             <div class="form-group col-6">
                                                 Subject
-                                                <input type="text" class="form-control" name="disc_sub" placeholder="Subject" id="disc_sub" value="">
+                                                <input type="text" class="form-control" name="disc_sub" placeholder="Subject" id="disc_sub" value="<?= $disc_sub; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 Name of Book
-                                                <input type="text" class="form-control" name="disc_book" id="disc_book" placeholder="Name of Book">
+                                                <input type="text" class="form-control" name="disc_book" id="disc_book" placeholder="Name of Book" value="<?= $disc_book; ?>">
                                             </div>
                                             <div class="form-group col-12">
                                                 <br>
                                                 Moderator
-                                                <input type="text" class="form-control" name="modrtr" id="modrtr" placeholder="Moderator">
+                                                <input type="text" class="form-control" name="modrtr" id="modrtr" placeholder="Moderator" value="<?= $modrtr; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 <br>
                                                 Particiapant 1
-                                                <input type="text" class="form-control" name="prtcpnt1" id="prtcpnt1" placeholder="Particiapant 1">
+                                                <input type="text" class="form-control" name="prtcpnt1" id="prtcpnt1" placeholder="Particiapant 1" value="<?= $prtcpnt1; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 <br>
                                                 Particiapant 2
-                                                <input type="text" class="form-control" name="prtcpnt2" id="prtcpnt2" placeholder="Particiapant 2">
+                                                <input type="text" class="form-control" name="prtcpnt2" id="prtcpnt2" placeholder="Particiapant 2" value="<?= $prtcpnt2; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 <br>
                                                 Particiapant 3
-                                                <input type="text" class="form-control" name="prtcpnt3" id="prtcpnt3" placeholder="Particiapant 3">
+                                                <input type="text" class="form-control" name="prtcpnt3" id="prtcpnt3" placeholder="Particiapant 3" value="<?= $prtcpnt3; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 <br>
                                                 Particiapant 4
-                                                <input type="text" class="form-control" name="prtcpnt4" id="prtcpnt4" placeholder="Particiapant 4">
+                                                <input type="text" class="form-control" name="prtcpnt4" id="prtcpnt4" placeholder="Particiapant 4" value="<?= $prtcpnt4; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 <br>
@@ -237,8 +185,13 @@ $user_id = $user['id'];
                                                 Proposed Event Date
                                                 <select class="form-control form-group" name="evnt_day" id="evnt_day" style="height:35px;" required="required">
                                                     <option value="0">Select Proposed Event Day</option>
-                                                    <?php foreach ($event_days as $event_day) { ?>
-                                                        <option value="<?= $event_day[0] ?>"><?= $event_day[1]; ?> - <?= $event_day[2]; ?></option>
+                                                    <?php foreach ($event_days as $event_day) {
+                                                        $evntDaySelect = "";
+                                                        if ($event_day[0] == $evnt_day) {
+                                                            $evntDaySelect = 'selected';
+                                                        }
+                                                    ?>
+                                                        <option value="<?= $event_day[0] ?>" <?= $evntDaySelect; ?>><?= $event_day[1]; ?> - <?= $event_day[2]; ?></option>
                                                     <?php } ?>
                                                 </select>
                                             </div>
@@ -254,13 +207,16 @@ $user_id = $user['id'];
                                                 Proposed Time Slot
                                                 <select class="form-control form-group" name="time_slot" id="time_slot" style="height:35px;" required="required">
                                                     <option value="0">Select Proposed Event Day</option>
-                                                    <?php foreach ($event_slots as $event_slot) { ?>
-                                                        <option value="<?= $event_slot[0] ?>"><?= $event_slot[1]; ?> - <?= $event_slot[2]; ?></option>
+                                                    <?php foreach ($event_slots as $event_slot) {
+                                                        $evntTmeSelect = "";
+                                                        if ($event_slot[0] == $time_slot) {
+                                                            $evntTmeSelect = 'selected';
+                                                        }
+                                                    ?>
+                                                        <option value="<?= $event_slot[0] ?>" <?= $evntTmeSelect; ?>><?= $event_slot[1]; ?> - <?= $event_slot[2]; ?></option>
                                                     <?php } ?>
                                                 </select>
                                             </div>
-
-
                                             <!-- <div class="col-6">
                                                 </br>
                                                 <label>Book Cover (Only JPG, JPEG, PNG files are allowed for uploads.)</label>
@@ -268,34 +224,37 @@ $user_id = $user['id'];
                                             <div class="form-group col-6">
                                                 </br>
                                                 Book Cover (Only JPG, JPEG, PNG files are allowed for uploads.)
-                                                <input type="file" class="form-control" name="disc_book_cover" id="disc_book_cover" placeholder="Upload Book Cover">
-                                                <!-- <label id="logo_lab">
-                                                    <img src="data:image/jpg;charset=utf8;base64,<?= $logo; ?>" height="70vh" id="logo_img" <?= $edit; ?>>
-                                                </label>
-                                                <span id="changelogo" onclick="changeLogo();" <?= $edit; ?>><u>Change Logo</u></span> -->
+                                                <input type="file" class="form-control" name="disc_book_cover" id="disc_book_cover" placeholder="Upload Book Cover" value="<?= $fileName; ?>">
+
+                                                <!--<span id="changelogo" onclick="changeLogo();" <?= $edit; ?>><u>Change Logo</u></span> -->
 
                                                 <!-- <input type="file" class="form-control" name="logo" id="logo" placeholder="*Upload Logo"> -->
                                             </div>
-                                            <div class="form-group col-6"></div>
+                                            <div class="form-group col-6">
+                                            </br></br>
+                                                <label>
+                                                    <?= $fileName; ?>
+                                                </label>
+                                            </div>
                                             <div class="form-group col-6">
                                                 </br>
                                                 Contact Person Name
-                                                <input type="text" class="form-control" name="cntct_name" id="cntct_name" placeholder="Contact Person Name">
+                                                <input type="text" class="form-control" name="cntct_name" id="cntct_name" placeholder="Contact Person Name" value="<?= $cntct_name; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 </br>
                                                 Contact Phone No.
-                                                <input type="text" class="form-control" name="cntct_phno" id="cntct_phno" placeholder="Contact Phone No.">
+                                                <input type="text" class="form-control" name="cntct_phno" id="cntct_phno" placeholder="Contact Phone No." value="<?= $cntct_phno; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 </br>
                                                 Contact Email Id.
-                                                <input type="email" class="form-control" name="cntct_mail" id="cntct_mail" placeholder="Contact Email Id.">
+                                                <input type="email" class="form-control" name="cntct_mail" id="cntct_mail" placeholder="Contact Email Id." value="<?= $cntct_mail; ?>">
                                             </div>
                                             <div class="form-group col-6">
                                                 </br>
                                                 Remarks
-                                                <input type="text" class="form-control" name="disc_remark" id="disc_remark" placeholder="Remarks">
+                                                <input type="text" class="form-control" name="disc_remark" id="disc_remark" placeholder="Remarks" value="<?= $disc_remark; ?>">
                                             </div>
                                         </div><br>
                                         <!-- <div class="col-12">
