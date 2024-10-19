b5e78edbd1d81fac8e804757994cc609fa5c4499
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-10-19 11:22:35 +0530

    banner changed speaker message added

--- components/about.php
+++ components/about.php
@@ -1,55 +1,66 @@
- <!-- ======= About Section ======= -->
- <section id="about" class="about">
-   <div class="container">
+<!-- ======= About Section ======= -->
+<section id="about" class="about">
+  <div class="container">
 
-     <div class="row no-gutters">
-       <div class="content col-xl-5 d-flex align-items-stretch" data-aos="fade-up">
-         <div class="content">
-           <h3>KLIBF-General Information</h3>
-           <p>
-           The Kerala Legislature International Book Festival is a coalescence of cultural economic and political developments.  The Book Festival encompasses the world.  Innovative ideas are discussed and shared to bring significant changes in our society.  A retreat from the mundane is the trigger behind this event.   Kerala is on the road to create more of its celebrated models. Our endeavor to create a better world will be futile unless our people are knowledgeable.  Reading helps to refine our contemplation and perception.  It is insignificant whether burgeoning thoughts come from a school kid, teenager or youth. Exhilarating visions soar from young minds and hence it is our prerogative to ensure the whole hearted participation of our youth.</p>
-           <a href="./about-inner-page.php" class="about-btn">About us <i class="bx bx-chevron-right"></i></a>
-         </div>
-       </div>
-       <div class="col-xl-7 d-flex align-items-stretch">
-         <div class="icon-boxes d-flex flex-column justify-content-center">
-           <div class="row">
-             <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100"> -->
-               <!-- <i class="bx bx-receipt"></i> -->
+    <div class="row no-gutters">
+      <div class="content col-xl-5 d-flex align-items-stretch" data-aos="fade-up">
+        <div class="content">
+          <h3>KLIBF Logo</h3>
+          <p>
+            Majority birds and books in various languages share an intriguing similarity in their role as vessels of cultural exchange, transcending borders and enriching socities.</p>
+          <a href="./about-inner-page.php" class="about-btn">About us <i class="bx bx-chevron-right"></i></a>
+        </div>
+      </div>
+      <div class="col-xl-7 d-flex align-items-stretch">
+        <div class="icon-boxes d-flex flex-column justify-content-center">
+          <div class="row">
+            <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100"> -->
+            <!-- <i class="bx bx-receipt"></i> -->
 
-               <!-- <h1 class="text-light"><a href="index.html"><span>KLIBF</span></a></h1> -->
-               <!-- Uncomment below if you prefer to use an image logo -->
-               <!-- <a href="index.html"><img src="assets/img/about-msg/Pinarayi_Vijyan_File__1__1200x.webp" alt="" class="img-fluid"></a>
+            <!-- <h1 class="text-light"><a href="index.html"><span>KLIBF</span></a></h1> -->
+            <!-- Uncomment below if you prefer to use an image logo -->
+            <!-- <a href="index.html"><img src="assets/img/about-msg/Pinarayi_Vijyan_File__1__1200x.webp" alt="" class="img-fluid"></a>
              
 
                <h4>Chief Minister's Words</h4>
                <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip <a href="speaker-message.php">Read More...</a></p>
              </div> -->
-             <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-               <!-- <i class="bx bx-cube-alt"></i> -->
-               <a href="index.html"><img src="assets/img/about-msg/spkr1.jpg" alt="" class="img-fluid"></a>
-               <h4>Speaker's Words</h4>
-               <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt <a href="speaker-message.php">Read More...</a></p>
-             </div>
-             <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300"> -->
-               <!-- <i class="bx bx-images"></i> -->
-               <!-- <a href="index.html"><img src="assets/img/about-msg/dyspkr.jpg" alt="" class="img-fluid"></a>
+            <div class="col-md- icon-box" data-aos="fade-up" data-aos-delay="200">
+              <!-- <i class="bx bx-cube-alt"></i> -->
+              <a href="index.html"><img src="assets/img/about-msg/spkr1.jpg" alt="" class="img-fluid"></a>
+              <h4>Speaker's Words</h4>
+              <p>The Kerala Legislature International Book Festival transcends socio economic cultural and political
+                barriers. The festival is a celebration of democracy incorporating novel thoughts from progressive
+                thinkers across the globe. Legislatures are law making bodies and this new tradition of conducting book
+                festival by a legislature is a leap towards enhancement, the courage to think different. Our society
+                vouches for evolution and transformation at a fast pace to improve the lives of the marginalised. Ground
+                breaking ideas emanate from reading and unless the reading habit is nourished, our thought and vision
+                will lose its sparkle. Let this Book Festival help to spread the light of knowledge. Let us pledge to
+                make this world a better place to live. Together we can create a space of equality and alliance to learn
+                and share new knowledge. I am sure the Kerala Legislature International Book Festival will be a
+                significant milestone cherished by future generations.
+                <!-- <a href="speaker-message.php">Read More...</a> -->
+              </p>
+            </div>
+            <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300"> -->
+            <!-- <i class="bx bx-images"></i> -->
+            <!-- <a href="index.html"><img src="assets/img/about-msg/dyspkr.jpg" alt="" class="img-fluid"></a>
                <h4>Deputy Speaker's Desk</h4>
                <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere <a href="speaker-message.php">Read More...</a></p>
              </div> -->
-             <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400"> -->
-               <!-- <i class="bx bx-shield"></i> -->
-               <!-- <a href="index.html"><img src="assets/img/about-msg/secretary.jpg" alt="" class="img-fluid"></a>
+            <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400"> -->
+            <!-- <i class="bx bx-shield"></i> -->
+            <!-- <a href="index.html"><img src="assets/img/about-msg/secretary.jpg" alt="" class="img-fluid"></a>
                <h4>Message from Secretary</h4>
                <p>Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta <a href="speaker-message.php">Read More...</a></p>
              </div> -->
-           </div>
-         </div>
-         <!-- End .content-->
-       </div>
-     </div>
+          </div>
+        </div>
+        <!-- End .content-->
+      </div>
+    </div>
 
-   </div>
- </section>
+  </div>
+</section>
 
- <!-- End About Section -->
+<!-- End About Section -->
