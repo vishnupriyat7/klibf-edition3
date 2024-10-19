566813a55d8960eb194d29d27b422d2e2bc57399
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-10-19 15:31:10 +0530

    about page, speaker message done previous edition menus done

--- components/about.php
+++ components/about.php
@@ -1,66 +1,44 @@
 <!-- ======= About Section ======= -->
 <section id="about" class="about">
   <div class="container">
-
     <div class="row no-gutters">
-      <div class="content col-xl-5 d-flex align-items-stretch" data-aos="fade-up">
+      <!-- KLIBF Logo Section (5 columns) -->
+      <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
         <div class="content">
-          <h3>KLIBF Logo</h3>
+          <h3>Inspiration Behind the Logo</h3>
           <p>
-            Majority birds and books in various languages share an intriguing similarity in their role as vessels of cultural exchange, transcending borders and enriching socities.</p>
-          <a href="./about-inner-page.php" class="about-btn">About us <i class="bx bx-chevron-right"></i></a>
+            Majority birds and books in various languages share an intriguing similarity in their role as vessels of cultural exchange, transcending borders and enriching societies. Just as migratory birds travel vast distances, bringing traces of their unique habitats and behaviours into new ecosystems, books written in different languages carry stories, philosophies, and ways of life from diverse cultures. These books, like the birds, land in foreign territories, where they become windows into different worlds, offering
+            inspiration and sparking curiosity. The give and take in both cases - birds share patterns of movement, survival skills and adaptability, and books provide insights into lifestyles, traditions, and beliefs - demonstrate a profound cultural exchange.
+          </p>
         </div>
       </div>
-      <div class="col-xl-7 d-flex align-items-stretch">
-        <div class="icon-boxes d-flex flex-column justify-content-center">
-          <div class="row">
-            <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100"> -->
-            <!-- <i class="bx bx-receipt"></i> -->
 
-            <!-- <h1 class="text-light"><a href="index.html"><span>KLIBF</span></a></h1> -->
-            <!-- Uncomment below if you prefer to use an image logo -->
-            <!-- <a href="index.html"><img src="assets/img/about-msg/Pinarayi_Vijyan_File__1__1200x.webp" alt="" class="img-fluid"></a>
-             
-
-               <h4>Chief Minister's Words</h4>
-               <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip <a href="speaker-message.php">Read More...</a></p>
-             </div> -->
-            <div class="col-md- icon-box" data-aos="fade-up" data-aos-delay="200">
-              <!-- <i class="bx bx-cube-alt"></i> -->
-              <a href="index.html"><img src="assets/img/about-msg/spkr1.jpg" alt="" class="img-fluid"></a>
-              <h4>Speaker's Words</h4>
-              <p>The Kerala Legislature International Book Festival transcends socio economic cultural and political
-                barriers. The festival is a celebration of democracy incorporating novel thoughts from progressive
-                thinkers across the globe. Legislatures are law making bodies and this new tradition of conducting book
-                festival by a legislature is a leap towards enhancement, the courage to think different. Our society
-                vouches for evolution and transformation at a fast pace to improve the lives of the marginalised. Ground
-                breaking ideas emanate from reading and unless the reading habit is nourished, our thought and vision
-                will lose its sparkle. Let this Book Festival help to spread the light of knowledge. Let us pledge to
-                make this world a better place to live. Together we can create a space of equality and alliance to learn
-                and share new knowledge. I am sure the Kerala Legislature International Book Festival will be a
-                significant milestone cherished by future generations.
-                <!-- <a href="speaker-message.php">Read More...</a> -->
-              </p>
-            </div>
-            <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300"> -->
-            <!-- <i class="bx bx-images"></i> -->
-            <!-- <a href="index.html"><img src="assets/img/about-msg/dyspkr.jpg" alt="" class="img-fluid"></a>
-               <h4>Deputy Speaker's Desk</h4>
-               <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere <a href="speaker-message.php">Read More...</a></p>
-             </div> -->
-            <!-- <div class="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400"> -->
-            <!-- <i class="bx bx-shield"></i> -->
-            <!-- <a href="index.html"><img src="assets/img/about-msg/secretary.jpg" alt="" class="img-fluid"></a>
-               <h4>Message from Secretary</h4>
-               <p>Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta <a href="speaker-message.php">Read More...</a></p>
-             </div> -->
-          </div>
+      <!-- Speaker's Words Section (7 columns) -->
+      <div class="col-md-6 row" data-aos="fade-up">
+        <div class="content">
+          <h3>Speaker's Words</h3>
+          <img src="assets/img/about-msg/spkr1.jpg" alt="" class="img-fluid">
+          <p>
+            The Kerala Legislature International Book Festival transcends socio-economic, cultural, and political
+            barriers. The festival is a celebration of democracy incorporating novel thoughts from progressive thinkers
+            across the globe.      
+            <!-- Legislatures are law-making bodies, and this new tradition of conducting book festivals by
+            a legislature is a leap towards enhancement, the courage to think differently. Our society vouches for
+            evolution and transformation at a fast pace to improve the lives of the marginalized. Ground-breaking ideas
+            emanate from reading, and unless the reading habit is nourished, our thought and vision will lose its
+            sparkle. Let this Book Festival help spread the light of knowledge. Let us pledge to make this world a
+            better place to live. Together, we can create a space of equality and alliance to learn and share new
+            knowledge. I am sure the Kerala Legislature International Book Festival will be a significant milestone
+            cherished by future generations. -->
+          </p> <a href="speaker-message-inner.php">Read More...</a>
         </div>
-        <!-- End .content-->
       </div>
+      <!-- Speaker's Image (6 columns) -->
+
     </div>
-
   </div>
+
 </section>
+
 
 <!-- End About Section -->
