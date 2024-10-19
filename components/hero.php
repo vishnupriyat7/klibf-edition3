3deb3a17ad5b235972dd69485c8b7c4c18cf94dc
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-10-19 15:38:17 +0530

    banner-styles-modified

--- components/hero.php
+++ components/hero.php
@@ -2,8 +2,8 @@
  <section id="hero">
    <div class="hero-container" data-aos="flip-down">
      <h1>Welcome to Kerala Legislature </h1>
-     <h2>International Book Festival</h2>
-     <h1>3rd Edition</h1>
+     <h1>International Book Festival</h1>
+     <h2>3rd Edition</h2>
      <h3>2025 Jan 7-13</h3>
      <!-- <h4>Stall Registration is open for Publishers ! Last Date: 20th August 2023 <a href="register.php"> <span class="horizontal-shake">  Click here</span> </a> to Register</h4> -->
   
