720cb31a3ad196daf3d95d42aa773e70fbadb70c
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-10-21 11:21:25 +0530

    hero page downbutton style modified

--- components/hero.php
+++ components/hero.php
@@ -7,7 +7,8 @@
      <h3>2025 Jan 7-13</h3>
      <!-- <h4>Stall Registration is open for Publishers ! Last Date: 20th August 2023 <a href="register.php"> <span class="horizontal-shake">  Click here</span> </a> to Register</h4> -->
   
-     <!-- <a href="#about" class="btn-get-started scrollto"><i class="bx bx-chevrons-down"></i></a> -->
+     <a href="#about" class="btn-get-started"><i class="bx bx-chevrons-down"></i></a>
    </div>
+
  </section>
  <!-- End Hero -->
