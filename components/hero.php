9191668b42c3c2c6989ffbe25094d451d164c3aa
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-10-21 17:01:19 +0530

    hero text background changed

--- components/hero.php
+++ components/hero.php
@@ -1,10 +1,10 @@
  <!-- ======= Hero Section ======= -->
  <section id="hero">
-   <div class="hero-container" data-aos="zoom-out-up">
+   <div class="hero-container wrapper" data-aos="zoom-out-up">
      <h1>Welcome to Kerala Legislature </h1>
      <h1>International Book Festival</h1>
-     <h2>3rd Edition</h2>
-     <h3>2025 Jan 7-13</h3>
+     <h2>3rd Edition </h2><br>
+<h2>2025 Jan 7-13</h2>
      <!-- <h4>Stall Registration is open for Publishers ! Last Date: 20th August 2023 <a href="register.php"> <span class="horizontal-shake">  Click here</span> </a> to Register</h4> -->
   
      <a href="#about" class="btn-get-started"><i class="bx bx-chevrons-down"></i></a>
