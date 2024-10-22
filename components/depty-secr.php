a87202df145546c698814b01aa335db161ad897c
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-10-22 11:12:21 +0530

    dyspeaker-msg-added

--- components/depty-secr.php
+++ components/depty-secr.php
@@ -8,9 +8,9 @@
                     <h3>Deputy Speaker's Words</h3>
                     <img src="assets/img/about-msg/dpy-spkr.jpeg" alt="" class="img-fluid">
                     <p>
-                        The Kerala Legislature International Book Festival transcends socio-economic, cultural, and political
-                        barriers. The festival is a celebration of democracy incorporating novel thoughts from progressive thinkers
-                        across the globe.
+                    I am thrilled to announce that the third edition of the prestigious Kerala Legislative
+              International Book Festival (KLIBF) will take place from 2025 January 7 to 13, at the
+              legislative complex in the historic capital of Kerala, Trivandrum.
                         <!-- Legislatures are law-making bodies, and this new tradition of conducting book festivals by
             a legislature is a leap towards enhancement, the courage to think differently. Our society vouches for
             evolution and transformation at a fast pace to improve the lives of the marginalized. Ground-breaking ideas
@@ -19,7 +19,7 @@
             better place to live. Together, we can create a space of equality and alliance to learn and share new
             knowledge. I am sure the Kerala Legislature International Book Festival will be a significant milestone
             cherished by future generations. -->
-                    </p> <a href="dyspeaker-message-innerpage.php">Read More...</a>
+                    </p> <a href="dyspeaker-message-inner.php">Read More...</a>
                 </div>
             </div>
 
--- dyspeaker-message-inner.php
+++ dyspeaker-message-inner.php
@@ -29,23 +29,33 @@
     <section class="about-inner-page">
       <div class="container text-justify text-lg-justify">
         <div class="row">
-        <div class="col-md-4">
-          <img src="assets/img/about-msg/dpy-spkr.jpeg" alt="" class="img-fluid"><br>
+          <div class="col-md-4">
+            <img src="assets/img/about-msg/dpy-spkr.jpeg" alt="" class="img-fluid"><br>
+          </div>
+          <div class="col-md-8">
+            <p>
+                         I am thrilled to announce that the third edition of the prestigious Kerala Legislative
+              International Book Festival (KLIBF) will take place from 2025 January 7 to 13, at the
+              legislative complex in the historic capital of Kerala, Trivandrum. The previous editions were highly
+              acclaimed for their public participation and the variety of cultural and artistic events. As part of the
+              second edition, hundreds of books were distributed to village and school libraries, further
+              establishing KLIBF as a renowned platform for celebrating the written word and an annual
+              pilgrimage for book lovers of all ages.<br>
+              Language and literature are the foundation of Kerala’s unique cultural identity. What defines
+              Kerala’s original identity is its Malayalam language and the literature born from it. Originating in
+              oral traditions, it evolved into inscriptions, manuscripts, print, and has now ventured into the digital
+              age. Malayalam language and literature have a rich and profound history, and with the guidance of
+              an outstanding array of poets and authors, they continue to spearhead modern literary development
+              in India.<br>
+              I invite everyone to the Kerala Niyamasabha fest, where in our land you can experience the
+              vibes of Kathakali, traditional Sopana music, and the thrilling rhythms of Panchavadyam, Chenda
+              Melam, and Thayambaka – a grand display of Kerala’s prestigious traditional musical instruments.
+              The festival also features ancient writings on the science of architecture, such as Tantra
+              Samuchayam and Shilpa Chandrika, celebrating Kerala's literary legacy over the centuries.
+              On this occasion, I extend my heartfelt greetings and best wishes for the success of the third
+              edition of the Kerala Niyamasabha International Book Festival.</p><br><br><br>
+          </div>
         </div>
-        <div class="col-md-8">
-          <p>
-            The Kerala Legislature International Book Festival transcends socio economic cultural and political
-            barriers. The festival is a celebration of democracy incorporating novel thoughts from progressive thinkers
-            across the globe. Legislatures are law making bodies and this new tradition of conducting book festival by a
-            legislature is a leap towards enhancement, the courage to think different. Our society vouches for evolution
-            and transformation at a fast pace to improve the lives of the marginalised. Ground breaking ideas emanate
-            from reading and unless the reading habit is nourished, our thought and vision will lose its sparkle. Let
-            this Book Festival help to spread the light of knowledge. Let us pledge to make this world a better place to
-            live. Together we can create a space of equality and alliance to learn and share new knowledge. I am sure
-            the Kerala Legislature International Book Festival will be a significant milestone cherished by future
-            generations.</p><br><br><br>
-        </div>
-      </div>
       </div>
     </section>
 
