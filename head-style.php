58f75c160644055d3596f3e3d6cb24c50f85b446
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-10-23 12:47:44 +0530

    title logo replaced with old

--- head-style.php
+++ head-style.php
@@ -7,8 +7,8 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <!-- <link href="assets/img/Logo_title.png" rel="icon"> -->
-  <link href="assets/img/Logo_KLIBF03.png" rel="icon">
+  <link href="assets/img/Logo_title.png" rel="icon">
+  <!-- <link href="assets/img/Logo_KLIBF03.png" rel="icon"> -->
   <!-- <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
 
 
