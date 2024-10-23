c4f5a1de6ac720b486f210feded5b32791d5da39
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-10-23 12:28:39 +0530

    logo description majority as migratory

--- components/logo-video.php
+++ components/logo-video.php
@@ -7,7 +7,7 @@
     </div>
     <div class="row no-gutters">
           <p class="logo-decsription">
-            Majority birds as well as books in various languages share an intriguing similarity in their role as vessels of cultural exchange, transcending borders and enriching societies. Just as migratory birds travel vast distances, bringing traces of their unique habitats and behaviours into new ecosystems, books written in different languages carry stories, philosophies, and ways of life from diverse cultures. These books, like the birds, land in foreign territories, where they become windows into different worlds, offering
+          Migratory birds as well as books in various languages share an intriguing similarity in their role as vessels of cultural exchange, transcending borders and enriching societies. Just as migratory birds travel vast distances, bringing traces of their unique habitats and behaviours into new ecosystems, books written in different languages carry stories, philosophies, and ways of life from diverse cultures. These books, like the birds, land in foreign territories, where they become windows into different worlds, offering
             inspiration and sparking curiosity. The give and take in both cases - birds share patterns of movement, survival skills and adaptability, and books provide insights into lifestyles, traditions, and beliefs - demonstrate a profound cultural exchange.
           </p>
       <!-- KLIBF Logo Section (5 columns) -->
