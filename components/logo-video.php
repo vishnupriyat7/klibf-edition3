6a6e5e8cd77d66611c093e5fe68aba951b5a1aad
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-10-23 10:54:22 +0530

    video decription added, cideo height adjusted

--- components/logo-video.php
+++ components/logo-video.php
@@ -2,12 +2,16 @@
 <section id="about" class="about">
   <div class="container">
     <div class="section-title">
-      <h2>Logo</h2>
+      <h2>Behind the Logo</h2>
       <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
     </div>
     <div class="row no-gutters">
+          <p class="logo-decsription">
+            Majority birds as well as books in various languages share an intriguing similarity in their role as vessels of cultural exchange, transcending borders and enriching societies. Just as migratory birds travel vast distances, bringing traces of their unique habitats and behaviours into new ecosystems, books written in different languages carry stories, philosophies, and ways of life from diverse cultures. These books, like the birds, land in foreign territories, where they become windows into different worlds, offering
+            inspiration and sparking curiosity. The give and take in both cases - birds share patterns of movement, survival skills and adaptability, and books provide insights into lifestyles, traditions, and beliefs - demonstrate a profound cultural exchange.
+          </p>
       <!-- KLIBF Logo Section (5 columns) -->
-      <video width="800" height="auto" controls>
+      <video class="logo-video" controls>
         <source src="assets/video/BOOK_FEST.mp4" type="video/mp4">
         <!-- <source src="movie.ogg" type="video/ogg"> -->
         Your browser does not support the video tag.
