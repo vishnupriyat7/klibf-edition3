e97059db0c888441a6baecee5cbad01990108163
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-10-23 12:46:18 +0530

    logo description majority as migratory-behind logo text changed

--- components/logo-video.php
+++ components/logo-video.php
@@ -6,10 +6,11 @@
       <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p> -->
     </div>
     <div class="row no-gutters">
-          <p class="logo-decsription">
-          Migratory birds as well as books in various languages share an intriguing similarity in their role as vessels of cultural exchange, transcending borders and enriching societies. Just as migratory birds travel vast distances, bringing traces of their unique habitats and behaviours into new ecosystems, books written in different languages carry stories, philosophies, and ways of life from diverse cultures. These books, like the birds, land in foreign territories, where they become windows into different worlds, offering
-            inspiration and sparking curiosity. The give and take in both cases - birds share patterns of movement, survival skills and adaptability, and books provide insights into lifestyles, traditions, and beliefs - demonstrate a profound cultural exchange.
-          </p>
+      <p class="logo-decsription">
+        Migratory birds as well as books in various languages share an intriguing similarity in their role as vessels of cultural exchange, transcending borders and enriching societies. Just as migratory birds travel vast distances, bringing traces of their unique habitats and behaviours into new ecosystems, books written in different languages carry stories, philosophies, and ways of life from diverse cultures. These books, like the birds, land in foreign territories, where they become windows into different worlds, offering
+        inspiration and sparking curiosity. The logo is inspired by this idea, with multiple colors representing cultural diversity around the world.
+        <br>
+      </p>
       <!-- KLIBF Logo Section (5 columns) -->
       <video class="logo-video" controls>
         <source src="assets/video/BOOK_FEST.mp4" type="video/mp4">
