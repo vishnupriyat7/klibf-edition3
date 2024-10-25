e003f728915ed0d3a9e1e2c103ee220675b2daa2
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-10-25 16:19:20 +0530

    login and register page modified

--- register.php
+++ register.php
@@ -103,9 +103,9 @@ if (isset($_POST['submit'])) {
                     <div class="alert-close">
                         <span class="fa fa-close"></span>
                     </div>
-                    <div class="w3l_form align-self">
+                    <div class="w3l_form">
                         <div class="left_grid_info">
-                            <img src="assets/img/images/Opt1_Reg.svg" alt="">
+                            <!-- <img src="assets/img/images/Opt1_Reg.svg" alt=""> -->
                         </div>
                     </div>
                     <div class="content-wthree">
