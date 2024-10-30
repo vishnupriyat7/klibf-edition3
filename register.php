a99ea76c3fa0e34fb686ef8e290c5f57a4a63fc1
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-10-30 16:10:56 +0530

    login and registration image and styles changed

--- register.php
+++ register.php
@@ -103,9 +103,9 @@ if (isset($_POST['submit'])) {
                     <div class="alert-close">
                         <span class="fa fa-close"></span>
                     </div>
-                    <div class="w3l_form">
+                    <div class="w3l_form register">
                         <div class="left_grid_info">
-                            <!-- <img src="assets/img/images/Opt1_Reg.svg" alt=""> -->
+                            <!-- <img src="assets/img/Reg.jpg" alt=""> -->
                         </div>
                     </div>
                     <div class="content-wthree">
