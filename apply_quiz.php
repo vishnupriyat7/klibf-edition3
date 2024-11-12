f6c37d6258217240f2ec25d7a0d0eba22a3ad1f9
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-11-12 15:50:19 +0530

    media&publicity-quiz-all-report-correction-made

--- apply_quiz.php
+++ apply_quiz.php
@@ -132,8 +132,8 @@ include "head-style.php";
                                             // }
                                             $sel_reg_quiz_qry = "SELECT id from reg_quiz where inst_prnci_cntct = '$principal_cntct'";
                                             $sel_reg_quiz_res = mysqli_query($conn, $sel_reg_quiz_qry);
-                                            if ($sel_reg_quiz_res->num_rows > 2) {
-                                                $msg .= "You have already registered 2 teams from this institution. More than 2 teams not allowed.<BR>";
+                                            if ($sel_reg_quiz_res->num_rows > 1) {
+                                                $msg .= "You have already registered 2 teams from this institution. More than 2 teams not allowed from the same institution.<BR>";
                                                 $status = "NOTOK";
                                             }
                                         }
