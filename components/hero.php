152951ad03db5ac9140dccdbcfb8d637c54d3d9a
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-11-16 16:25:39 +0530

    stall registration date extend message yupdated in home page

--- components/hero.php
+++ components/hero.php
@@ -5,7 +5,7 @@
     <h1>International Book Festival</h1><br>
     <h2>3rd Edition </h2>
     <h2>2025 JANUARY 7-13</h2>
-    <h4>Stall Registration for Publishers<a href="register.php"> <span class="horizontal-shake">  Click here</span> </a> to Register</h4>
+    <h4>Stall Registration for Publishers Extended to November 20, 2024. <a href="register.php"> <span class="horizontal-shake"> Click Here</span> </a> to Register</h4>
     <a href="#about" class="btn-get-started"><i class="bx bx-chevrons-down"></i></a>
   </div>
 
