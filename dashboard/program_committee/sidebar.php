219b3282ca736ded24b86154d6df2508f460a62c
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-11-20 15:39:33 +0530

    stall allotment for program committee

--- dashboard/program_committee/sidebar.php
+++ dashboard/program_committee/sidebar.php
@@ -45,6 +45,12 @@
                         <i class="ri-file-list-3-line"></i> <span data-key="t-landing">Terms and Conditions</span>
                     </a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link menu-link" role="button" aria-controls="sidebarLanding"
+                        href="<?= $base_url ?>/dashboard/program_committee/stall_allot_list.php" class="nav-link" data-key="t-one-page">
+                        <i class="ri-file-list-3-line"></i> <span data-key="t-landing">Stall Allotment</span>
+                    </a>
+                </li>
 
                 <li class="nav-item">
                     <a class="nav-link menu-link" href="#sidebarX" data-bs-toggle="collapse" role="button"
