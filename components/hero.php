7020fc895a3d11b2d0f08d208ad2c9029ff70f69
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-11-20 11:53:26 +0530

    img-file-upload-processing

--- components/hero.php
+++ components/hero.php
@@ -7,6 +7,9 @@
     <h2>2025 JANUARY 7-13</h2>
     <h4>Stall Registration for Publishers Extended to November 20, 2024. <a href="register.php"> <span class="horizontal-shake"> Click Here</span> </a> to Register</h4>
     <a href="#about" class="btn-get-started"><i class="bx bx-chevrons-down"></i></a>
+
+    <h4>For Contests  <a href="#contests"> <span class="horizontal-shake" style="color: red;"> Click Here</span> </a> to Register</h4>
+    <a href="#about" class="btn-get-started"><i class="bx bx-chevrons-down"></i></a>
   </div>
 
 </section>
