39c8043aa6057895749863365e7d193adb3be20b
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2024-11-23 17:41:04 +0530

    quiz extended date message uncomment parvathi

--- contest-quiz-registration.php
+++ contest-quiz-registration.php
@@ -210,7 +210,7 @@
                                     </div>
                                     <div class="align-items-center text-center">
                                         <h3><b>ക്വിസ് മത്സരം - പൊതുമാർഗ്ഗനിർദ്ദേശങ്ങൾ</b></h3>
-                                        <!-- <p class="text-danger"><b>കണ്ണൂർ മേഖലാതല ക്വിസ് രജിസ്‌ട്രേഷൻ 26-11-2024 വരെ ദീർഘിപ്പിച്ചിരിക്കുന്നു.</b></p> -->
+                                        <p class="text-danger"><b>കണ്ണൂർ മേഖലാതല ക്വിസ് രജിസ്‌ട്രേഷൻ 26-11-2024 വരെ ദീർഘിപ്പിച്ചിരിക്കുന്നു.</b></p>
                                     </div>
 
 
@@ -342,7 +342,7 @@
                                                             </td>
                                                             <td data-label="മേഖല">കണ്ണൂര്‍</td>
                                                             <td data-label="ഓൺലൈൻ രജിസ്ട്രേഷനുള്ള അവസാന തീയതി">22.11.2024
-                                                                <!-- <p class="text-danger fw-bold">Extended Date</p>26.11.2024 -->
+                                                                <p class="text-danger fw-bold">Extended Date<br>26.11.2024</p>
                                                             </td>
                                                             <td data-label="പ്രാഥമിക മത്സര തീയതി">29.11.2024</td>
                                                             <td data-label="വേദി">ശിക്ഷക് സദൻ , കണ്ണൂർ</td>
