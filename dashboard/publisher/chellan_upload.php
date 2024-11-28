8424fa22f7c33074b91386f7f6e2ced5e1602812
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-11-28 17:13:45 +0530

    invoice print done

--- dashboard/publisher/chellan_upload.php
+++ dashboard/publisher/chellan_upload.php
@@ -97,22 +97,30 @@ function convertNumberToWordsForIndia($number)
 }
 function generateInvoice($invoiceNo)
 {
-    $alphaCode = array(
-        "1" => "A",
-        "2" => "B",
-        "3" => "C",
-        "4" => "D",
-        "5" => "E",
-        "6" => "F",
-        "7" => "G",
-        "8" => "H",
-        "9" => "I",
-        "0" => "J"
-    );
-    $currentDate = new DateTime();
-    $year = $currentDate->format("y");
-    $arr1 = str_split($year);
-    $generatedNo = $alphaCode[$arr1[0]] . $alphaCode[$arr1[1]] . $invoiceNo;
+    // $alphaCode = array(
+    //     "1" => "A",
+    //     "2" => "B",
+    //     "3" => "C",
+    //     "4" => "D",
+    //     "5" => "E",
+    //     "6" => "F",
+    //     "7" => "G",
+    //     "8" => "H",
+    //     "9" => "I",
+    //     "0" => "J"
+    // );
+    // $currentDate = new DateTime();
+    // $year = $currentDate->format("y");
+    // $arr1 = str_split($year);
+    // $generatedNo = $alphaCode[$arr1[0]] . $alphaCode[$arr1[1]] . $invoiceNo;
+    // return $generatedNo;
+    if (strlen((string) $invoiceNo) == 1) {
+        $generatedNo = 'KLIBF III-01-2025-000' . (string) $invoiceNo;
+    } else if (strlen((string) $invoiceNo) == 2) {
+        $generatedNo = 'KLIBF III-01-2025-00' . (string) $invoiceNo;
+    } else {
+        $generatedNo = 'KLIBF III-01-2025-0' . (string) $invoiceNo;
+    }
     return $generatedNo;
 }
 ?>
@@ -550,6 +558,7 @@ function generateInvoice($invoiceNo)
         <!-- container-fluid -->
     </div>
     <iframe id="print-frame" style="display: none;"></iframe>
+    <iframe id="print-invoice-frame" style="display: none;"></iframe>
     <!-- End Page-content -->
 
     <?php include "../footer.php"; ?>
@@ -612,69 +621,42 @@ function generateInvoice($invoiceNo)
             var slno = 0;
             var desc3x3 = "";
             var desc3x2 = "";
-            var divToPrint = document.getElementById("pay-slip");
-            newWin = window.open("");
-            newWin.document.write('<html><head> <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" /><link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" /><link href="assets/css/app.min.css" rel="stylesheet" type="text/css" /><link href="assets/css/custom.min.css" rel="stylesheet" type="text/css" /></head><body><style>td, th {font-size:12;} .center {display: block; margin-left: auto; margin-right: auto;width: auto; }</style>');
-            newWin.document.write('<br><div class="text-center"><img src="<?= $base_url; ?>/dashboard/assets/images/Govt_Logo.png" height="70vh" class="center"><br></div><table><tr><th colspan="11" class="text-center">SECRETARIAT OF THE KERALA LEGISLATURE<br></th></tr><tr><td>Post Box No:</td><td>5430</td><td colspan="6"></td><td>GSTN: </td><td><b>32AAAGK0786J1ZD</b></td></tr><tr><td>PIN:</td><td>695 033</td></tr><tr><td>Email:</td><td>secretary@niyamasabha.nic.in</td></tr><tr><th class="text-center" colspan="11"><br><br><br><br>INVOICE<br></th></tr><tr><td rowspan="2">Bill To</td><td rowspan="2">');
-            newWin.document.write(<?= json_encode($user_prof['org_name']); ?>);
-            newWin.document.write('<br>');
-            newWin.document.write(<?= json_encode($user_prof['head_org_email']); ?>);
-            newWin.document.write('</td><td colspan="4"></td><td colspan="3">Invoice No</td><td>');
-            newWin.document.write(<?= json_encode($invoice); ?>);
-            newWin.document.write('</td></tr><tr><td colspan="4"></td><td colspan="3">Invoice Date</td><td>');
-            newWin.document.write(<?= json_encode($chellan['updated_date']); ?>);
-            newWin.document.write('</td></tr><tr><td>GSTIN: </td><td>');
-            newWin.document.write(<?= json_encode($user_prof['gst_no']) ?>);
-            newWin.document.write('</td></tr><tr><td colspan="11"><br><br></td></tr><tr><td rowspan="2" style="text-align: center;">No</td><td rowspan="2" style="text-align: center;">Item Description</td><td rowspan="2" style="text-align: center;">HSN/<br>SAC</td><td rowspan="2" style="text-align: center;">Qty</td><td rowspan="2" style="text-align: center;">Unit Price</td><td rowspan="2" style="text-align: center;">Taxable Amount</td><td colspan="3" style="text-align: center;">GST</td><td rowspan="2" style="text-align: center;">Total</td></tr><tr><td style="text-align: center;">%</td><td style="text-align: center;">SGST</td><td style="text-align: center;">CGST</td></tr><tr><td style="text-align: center;">');
+            // var divToPrint = document.getElementById("pay-slip");
+
+            var htmlContent = '<html>';
+            var htmlContent = htmlContent + '<head>';
+            var htmlContent = htmlContent + '<link href="<?= $base_url; ?>/dashboard/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />';
+            var htmlContent = htmlContent + '<link href="<?= $base_url; ?>/dashboard/assets/css/icons.min.css" rel="stylesheet" type="text/css" />';
+            var htmlContent = htmlContent + '<link href="<?= $base_url; ?>/dashboard/assets/css/app.min.css" rel="stylesheet" type="text/css" />';
+            var htmlContent = htmlContent + '<link href="<?= $base_url; ?>/dashboard/assets/css/custom.min.css" rel="stylesheet" type="text/css" />';
+            var htmlContent = htmlContent + '</head>';
+            var htmlContent = htmlContent + '<body>';
+            var htmlContent = htmlContent + '<style>td, th {font-size:12;} .center {display: block; margin-left: auto; margin-right: auto;width: auto; }</style>';
+            var htmlContent = htmlContent + '<br><div class="text-center"><img src="<?= $base_url; ?>/dashboard/assets/images/Govt_Logo.png" height="70vh" class="center"><br></div>';
+            var htmlContent = htmlContent + '<table class="table table-striped table-bordered">';
+            var htmlContent = htmlContent + '<tr><th colspan="11" class="text-center">SECRETARIAT OF THE KERALA LEGISLATURE<br></th></tr>';
+            var htmlContent = htmlContent + '<tr><td>PB No:</td><td>5430</td><td colspan="6"></td><td>GSTN: </td><td><b>32AAAGK0786J1ZD</b></td></tr>';
+            var htmlContent = htmlContent + '<tr><td>PIN:</td><td>695 033</td></tr><tr><td>Email:</td><td>secretary@niyamasabha.nic.in</td></tr>';
+            var htmlContent = htmlContent + '<tr><th class="text-center" colspan="11">INVOICE<br></th></tr>';
+            var htmlContent = htmlContent + '<tr><td>Bill To</td><td><?= $user_prof['org_name']; ?><br><?= $user_prof['head_org_email']; ?></td><td colspan="3"></td><td colspan="3" style="text-align: right;">Invoice No:</td><td colspan="2"><?= $invoice; ?></td></tr>';
+            var htmlContent = htmlContent + '<tr><td>GSTIN:</td><td><?= $user_prof['gst_no']; ?></td><td colspan="3"></td><td colspan="3" style="text-align: right;">Invoice Date:</td><td colspan="2"><?= $chellan['approved_date']; ?></td></tr>';
+            var htmlContent = htmlContent + '<tr><td colspan="11"></td></tr>';
+            var htmlContent = htmlContent + '<tr><td rowspan="2" style="text-align: center;">No</td><td rowspan="2" style="text-align: center;">Item Description</td><td rowspan="2" style="text-align: center;">HSN/SAC</td><td rowspan="2" style="text-align: center;">Qty</td><td rowspan="2" style="text-align: center;">Unit Price</td><td rowspan="2" style="text-align: right;">Taxable Amount</td><td colspan="3" style="text-align: center;">GST</td><td rowspan="2" style="text-align: right;">Total</td></tr>';
+            var htmlContent = htmlContent + '<tr><td style="text-align: center;">%</td><td style="text-align: center;">SGST</td><td style="text-align: center;">CGST</td></tr><tr><td style="text-align: center;">';
 
             if (stall3x3 > 0) {
-                newWin.document.write(++slno);
-                newWin.document.write('</td><td>Rent for Stall 3x3m 01/11/2023-07/11/2023</td><td>997222</td><td style="text-align: right;">&emsp;');
-                newWin.document.write(stall3x3);
-                newWin.document.write('</td><td style="text-align: right;">10000</td><td style="text-align: right;">');
-                newWin.document.write(<?= json_encode($rate3x3) ?>);
-                newWin.document.write('</td><td style="text-align: right;">18</td><td style="text-align: right;">');
-                newWin.document.write(<?= json_encode($gst3x3 / 2) ?>);
-                newWin.document.write('</td><td style="text-align: right;">');
-                newWin.document.write(<?= json_encode($gst3x3 / 2) ?>);
-                newWin.document.write('</td><td style="text-align: right;">');
-                newWin.document.write(<?= json_encode($tot_amt3x3) ?>);
+                var htmlContent = htmlContent + (++slno) + '</td><td>Rent for Stall 3x3m 01/11/2023-07/11/2023</td><td>997222</td><td style="text-align: right;">&emsp;' + stall3x3 + '</td><td style="text-align: right;">10000</td><td style="text-align: right;"><?= $rate3x3; ?></td><td style="text-align: right;">18</td><td style="text-align: right;"><?= ($gst3x3 / 2); ?></td><td style="text-align: right;"><?= ($gst3x3 / 2); ?></td><td style="text-align: right;"><?= $tot_amt3x3; ?>';
             }
+
             if (stall3x2 > 0) {
-                newWin.document.write(++slno);
-                newWin.document.write('</td><td>Rent for Stall 3x2m 01/11/2023-07/11/2023</td><td>997222</td><td style="text-align: right;">');
-                newWin.document.write(stall3x2);
-                newWin.document.write('</td><td style="text-align: right;">8500</td><td style="text-align: right;">');
-                newWin.document.write(<?= json_encode($rate3x2) ?>);
-                newWin.document.write('</td><td style="text-align: right;">18</td><td style="text-align: right;">');
-                newWin.document.write(<?= json_encode($gst3x2 / 2) ?>);
-                newWin.document.write('</td><td style="text-align: right;">');
-                newWin.document.write(<?= json_encode($gst3x2 / 2) ?>);
-                newWin.document.write('</td><td style="text-align: right;">');
-                newWin.document.write(<?= json_encode($tot_amt3x2) ?>);
+                var htmlContent = htmlContent + (++slno) + '</td><td>Rent for Stall 3x2m 01/11/2023-07/11/2023</td><td>997222</td><td style="text-align: right;">' + stall3x2 + '</td><td style="text-align: right;">8500</td><td style="text-align: right;"><?= $rate3x2; ?></td><td style="text-align: right;">18</td><td style="text-align: right;"><?= ($gst3x2 / 2); ?></td><td style="text-align: right;"><?= ($gst3x2 / 2); ?></td><td style="text-align: right;"><?= $tot_amt3x2; ?>';
             }
-            newWin.document.write('</td></tr><tr><td colspan="11"><br><br></td></tr><tr><td colspan="3" style="text-align: right;">Total</td><td style="text-align: right;">');
-            newWin.document.write(stall3x3 + stall3x2);
-            newWin.document.write('</td><td></td><td style="text-align: right;">');
-            newWin.document.write(<?= json_encode($rate3x3 + $rate3x2) ?>);
-            newWin.document.write('</td><td></td><td style="text-align: right;">');
-            newWin.document.write(<?= json_encode(($gst3x3 + $gst3x2) / 2) ?>);
-            newWin.document.write('</td><td style="text-align: right;">');
-            newWin.document.write(<?= json_encode(($gst3x3 + $gst3x2) / 2) ?>);
-            newWin.document.write('</td><td style="text-align: right;">');
-            newWin.document.write(<?= json_encode($total_amt) ?>);
-            newWin.document.write('</td></tr><tr><td colspan="11"><br><br></td></tr><tr><td colspan="4" style="text-align: right;">Total Taxable Amount</td><td colspan="7" style="text-align: right;">');
-            newWin.document.write(<?= json_encode($rate3x3 + $rate3x2) ?>);
-            newWin.document.write('</td></tr><tr><td colspan="4" style="text-align: right;">Total Tax Amount</td><td colspan="7" style="text-align: right;">');
-            newWin.document.write(<?= json_encode(($gst3x3 + $gst3x2)) ?>);
-            newWin.document.write('</td></tr><tr><td colspan="4" style="text-align: right;">Total Amount</td><td colspan="7" style="text-align: right;">');
-            newWin.document.write(<?= json_encode($total_amt) ?>);
-            newWin.document.write('</td></tr><tr><td colspan="4" style="text-align: right;">Amount Due</td><td colspan="7" style="text-align: right;">');
-            newWin.document.write(<?= json_encode($total_amt) ?>);
-            newWin.document.write('</td></tr><tr><td colspan="4" style="text-align: right;">Total (in words)</td><td colspan="7" style="text-align: right;">');
-            newWin.document.write(<?= json_encode($totalinword) ?>);
-            newWin.document.write('</td></tr><tr><td colspan="11" style="text-align: right;"><br><br><br><br><br><br><br>Authorized Signatory</td></tr></table></body></html>');
-            newWin.print();
-            newWin.close();
+            var htmlContent = htmlContent + '</td></tr><tr><td colspan="11"></td></tr><tr><td colspan="3" style="text-align: right;">Total</td><td style="text-align: right;">' + (stall3x3 + stall3x2) + '</td><td></td><td style="text-align: right;"><?= ($rate3x3 + $rate3x2); ?></td><td></td><td style="text-align: right;"><?= (($gst3x3 + $gst3x2) / 2); ?></td><td style="text-align: right;"><?= (($gst3x3 + $gst3x2) / 2); ?></td><td style="text-align: right;"><?= $total_amt; ?></td></tr><tr><td colspan="11"></td></tr><tr><td colspan="4" style="text-align: right;">Total Taxable Amount</td><td colspan="7" style="text-align: right;"><?= ($rate3x3 + $rate3x2); ?></td></tr><tr><td colspan="4" style="text-align: right;">Total Tax Amount</td><td colspan="7" style="text-align: right;"><?= ($gst3x3 + $gst3x2); ?></td></tr><tr><td colspan="4" style="text-align: right;">Total Amount</td><td colspan="7" style="text-align: right;"><?= $total_amt; ?></td></tr><tr><td colspan="4" style="text-align: right;">Amount Due</td><td colspan="7" style="text-align: right;"><?= $total_amt; ?></td></tr><tr><td colspan="4" style="text-align: right;">Total (in words)</td><td colspan="7" style="text-align: right;"><?= $totalinword; ?></td></tr><tr><td colspan="11" style="text-align: right;"><br><br><br>Authorized Signatory</td></tr></table></body></html>';
+
+            var iframe = document.getElementById("print-invoice-frame");
+            iframe.contentDocument.write(htmlContent);
+            iframe.contentDocument.close();
+            iframe.focus(); // Optional: focus on the iframe
+            iframe.contentWindow.print();
         }
     </script>
