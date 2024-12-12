1a9e2692d5db806d7eebc15ee0c850516d4a7580
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2024-12-12 14:49:41 +0530

    finance approve for already approved users

--- dashboard/finance_committee/payment_approve.php
+++ dashboard/finance_committee/payment_approve.php
@@ -8,6 +8,13 @@ $max_value_dtls = mysqli_query($con, $max_value);
 $res_max_value = mysqli_fetch_row($max_value_dtls);
 $invoice_maxval = $res_max_value[0];
 $next_invoice_val = (int) $invoice_maxval + 1;
-$updateQuery = "UPDATE challan SET status = 'A', approved_date = '$date', invoice_no = '$next_invoice_val' WHERE id='$chellanId'";
+$get_invoice_query = "SELECT invoice_no FROM challan WHERE id = '$chellanId'";
+$cehllan_invoice_result = mysqli_query($con, $get_invoice_query);
+$chellan_invoice_no = $cehllan_invoice_result->fetch_assoc();
+if ($chellan_invoice_no['invoice_no']) {
+    $updateQuery = "UPDATE challan SET status = 'A', approved_date = '$date' WHERE id='$chellanId'";
+} else {
+    $updateQuery = "UPDATE challan SET status = 'A', approved_date = '$date', invoice_no = '$next_invoice_val' WHERE id='$chellanId'";
+}
 $result = mysqli_query($con, $updateQuery);
 echo json_encode($result);
--- dashboard/sdf_committee/coupon_sponsers_details.php
+++ dashboard/sdf_committee/coupon_sponsers_details.php
@@ -136,7 +136,6 @@ function generateInvoice($invoiceNo)
 <div class="main-content">
     <div class="page-content">
         <div class="container-fluid">
-
             <!-- start page title -->
             <div class="row">
                 <div class="col-12">
@@ -156,10 +155,8 @@ function generateInvoice($invoiceNo)
                 </div>
             </div>
             <!-- end page title -->
-
             <br><br>
             <div class="row">
-
                 <!--end col-->
                 <div class="col-xxl-9">
                     <div class="card mt-xxl-n5">
@@ -178,56 +175,64 @@ function generateInvoice($invoiceNo)
                             $spnsr_trnctn_dt = mysqli_real_escape_string($con, $_POST['spnsr_trnctn_dt']);
                             $current_date = new DateTime();
                             $date = date_format($current_date, "Y-m-d H:i:s");
-                            if ($status == "NOTOK") {
-                                $errormsg = "<div class='alert alert-danger alert-dismissible alert-outline fade show'>" .
-                                    $msg . "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
-                                               </div>"; //printing error if found in validation
-                            } else {
+                            $con->begin_transaction();
+                            try {
                                 $query_sponser = "INSERT INTO coupon_sponsers (spnsr_org_name, spnsr_amt, pay_mode_id, other_remark, trnctn_no, bnk_ref_no, trnct_dt, updated_date) VALUES ('$spnsr_org_name', '$spnsr_tot_amt', '$spnsr_trnctn_type', '$spnsr_pay_other', '$spnsr_trnctn_no', '$spnsr_bnk_ref_no', '$spnsr_trnctn_dt', '$date');";
-                            }
-                            $result_sponser = mysqli_query($con, $query_sponser);
-                            if ($result_sponser) {
-                                $last_id = mysqli_insert_id($con);
-                                $denominations = $_POST['spnsr_cpn_deno'];
-                                $serials_from = $_POST['spnsr_cpn_slno_frm'];
-                                $serials_to = $_POST['spnsr_cpn_slno_to'];
-                                $amounts = $_POST['spnsr_cpn_deno_amt'];
-                                // Loop through and insert into the database
-                                for ($i = 0; $i < count($denominations); $i++) {
-                                    $denomination_id = mysqli_real_escape_string($con, $denominations[$i]);
-                                    $serial_no_from = mysqli_real_escape_string($con, $serials_from[$i]);
-                                    $serial_no_to = mysqli_real_escape_string($con, $serials_to[$i]);
-                                    var_dump($serial_no_from);
-                                    var_dump($serial_no_to);
-                                    $total_coupons = ($serial_no_to - $serial_no_from) + 1;
-                                    for ($j = 0; $j < $total_coupons; $j++) {
-                                        $coupon_serial_no = $serial_no_from + $j;
-                                        $query_sponser_coupon = "INSERT INTO coupon_distribution (sponser_id, denom_id, serial_no, updated_date) VALUES ('$last_id', '$denomination_id', '$coupon_serial_no', '$date');";
-                                        $result_sponser_coupon = mysqli_query($con, $query_sponser_coupon);
-                                        if (!$result_sponser_coupon) {
-                                            $status = "NOTOK";
-                                            $msg = "Something went wrong!";
+                                // $result_sponser = mysqli_query($con, $query_sponser);
+                                if ($con->query($query_sponser)) {
+                                    $last_id = mysqli_insert_id($con);
+                                    $denominations = $_POST['spnsr_cpn_deno'];
+                                    $serials_from = $_POST['spnsr_cpn_slno_frm'];
+                                    $serials_to = $_POST['spnsr_cpn_slno_to'];
+                                    $amounts = $_POST['spnsr_cpn_deno_amt'];
+                                    $tot_deno_amt = 0;
+                                    // Loop through and insert into the database
+                                    for ($i = 0; $i < count($denominations); $i++) {
+                                        $denomination_id = mysqli_real_escape_string($con, $denominations[$i]);
+                                        $serial_no_from = mysqli_real_escape_string($con, $serials_from[$i]);
+                                        $serial_no_to = mysqli_real_escape_string($con, $serials_to[$i]);
+                                        $deno_amt = mysqli_real_escape_string($con, $amounts[$i]);
+                                        $tot_deno_amt = $tot_deno_amt + (int) $deno_amt;
+                                        $total_coupons = ($serial_no_to - $serial_no_from) + 1;
+                                        for ($j = 0; $j < $total_coupons; $j++) {
+                                            $coupon_serial_no = $serial_no_from + $j;
+                                            $query_sponser_coupon = "INSERT INTO coupon_distribution (sponser_id, denom_id, serial_no, updated_date) VALUES ('$last_id', '$denomination_id', '$coupon_serial_no', '$date');";
+                                            $result_sponser_coupon = mysqli_query($con, $query_sponser_coupon);
+                                            if (!$con->query($result_sponser_coupon)) {
+                                                $status = "NOTOK";
+                                                throw new Exception("Query failed: " . $con->error);
+                                            }
                                         }
                                     }
-                                    // $amount = mysqli_real_escape_string($con, $amounts[$i]);
-                                }
-                                $errormsg = "";
-                                if ($status == "NOTOK") {
-                                    $errormsg = "<div class='alert alert-danger alert-dismissible alert-outline fade show'>" .
-                                        $msg . "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                                    if ($tot_deno_amt != (int) $spnsr_tot_amt) {
+                                        $status = "NOTOK";
+                                        $msg = "Total amount missmatch";
+                                    }
+                                    $errormsg = "";
+                                    if ($status == "NOTOK") {
+                                        $errormsg = "<div class='alert alert-danger alert-dismissible alert-outline fade show'>" .
+                                            $msg . "<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                                                </div>"; //printing error if found in validation
-                                } else {
-                                    $errormsg = "<div class='alert alert-success alert-dismissible alert-outline fade show'>
+                                    } else {
+                                        $con->commit();
+                                        $errormsg = "<div class='alert alert-success alert-dismissible alert-outline fade show'>
                                             Your Coupon Sponser details is Successfully Saved.
                                             <button type='button' class='btn-close' data-dismiss='alert' aria-label='Close'></button>
                                             </div>";
-                                }
-                            } else {
-                                $errormsg = "
-                            <div class='alert alert-danger alert-dismissible alert-outline fade show'>
+                                    }
+                                } else {
+                                    $errormsg = "<div class='alert alert-danger alert-dismissible alert-outline fade show'>
                                        Some Technical Glitch Is There. Please Try Again Later Or Ask Admin For Help test.
                                        <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
                                        </div>";
+                                    throw new Exception("Query failed: " . $con->error);
+                                }
+                            } catch (Exception $e) {
+                                $con->rollback();
+                                $errormsg = "<div class='alert alert-danger alert-dismissible alert-outline fade show'>
+                                       Some Technical Glitch Is There. Please Try Again Later Or Ask Admin For Help test.
+                                       <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                                       </div>" . $e->getMessage();
                             }
                         }
                         ?>
