ce46f22c54c1fae6eb0fd70f95a6a42b847ebfb8
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2025-01-11 18:53:26 +0530

    report for submission doing

--- dashboard/publisher/coupon_sdf_report.php
+++ dashboard/publisher/coupon_sdf_report.php
@@ -27,7 +27,11 @@ $user_id = $user['id']; ?>
                 </div>
             </div>
             <!-- end page title -->
-
+            <?php
+            $total_coupon_invoice_query = "SELECT * FROM coupon_publisher_invoice WHERE user_id = '$user_id';";
+            $total_bills = mysqli_query($con, $total_coupon_invoice_query);
+            $counter = 0;
+            ?>
             <div class="row">
                 <div class="col-lg-12">
                     <div class="card">
@@ -37,9 +41,15 @@ $user_id = $user['id']; ?>
                         <div class="card-body overflow-auto">
                             <div class="row text-center">
                                 <div class="col-lg-12">
-                                    <button type="submit" name="cpn_report" id="cpn_report" class="btn btn-success" onclick="generateSdfReport()"
-                                        disabled>Generate Coupon Report</button>
+                                    <!-- <button type="submit" name="cpn_report" id="cpn_report" class="btn btn-success" onclick="generateSdfReport()"
+                                        disabled>Generate Coupon Report</button> -->
+
+
+                                    <button type="button" id="cpn_report" class="btn btn-success" onclick="generatePdfReport()">Generate Coupon Report</button>
                                 </div>
+
+
+
                             </div>
                             <!-- <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle" style="width:100%"> -->
 
@@ -51,57 +61,44 @@ $user_id = $user['id']; ?>
             <!--end row-->
         </div>
         <!-- container-fluid -->
-        <!-- <iframe id="print-frame" style="display: none;"></iframe> -->
+
         <iframe id="print-invoice-frame" style="display: none;"></iframe>
     </div>
     <!-- End Page-content -->
     <?php include "../footer.php"; ?>
     <script>
-        function generateSdfReport() {
-            // alert("hii");
-            var stall3x3 = <?= json_encode($stall3x3) ?>;
-            // alert(stall3x3);
-            var stall3x2 = <?= json_encode($stall3x2) ?>;
-            // alert(stall3x2);
-            var slno = 0;
-            var desc3x3 = "";
-            var desc3x2 = "";
-            // var divToPrint = document.getElementById("pay-slip");
+        function generatePdfReport() {
+            const userId = "<?= $user['id'] ?>"; // Pass the user_id dynamically
 
-            var htmlContent = '<html>';
-            var htmlContent = htmlContent + '<head>';
-            var htmlContent = htmlContent + '<link href="<?= $base_url; ?>/dashboard/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />';
-            var htmlContent = htmlContent + '<link href="<?= $base_url; ?>/dashboard/assets/css/icons.min.css" rel="stylesheet" type="text/css" />';
-            var htmlContent = htmlContent + '<link href="<?= $base_url; ?>/dashboard/assets/css/app.min.css" rel="stylesheet" type="text/css" />';
-            var htmlContent = htmlContent + '<link href="<?= $base_url; ?>/dashboard/assets/css/custom.min.css" rel="stylesheet" type="text/css" />';
-            var htmlContent = htmlContent + '</head>';
-            var htmlContent = htmlContent + '<body>';
-            var htmlContent = htmlContent + '<style>td, th {font-size:12;} .center {display: block; margin-left: auto; margin-right: auto;width: auto; }</style>';
-            var htmlContent = htmlContent + '<br><div class="text-center"><img src="<?= $base_url; ?>/dashboard/assets/images/Govt_Logo.png" height="70vh" class="center"><br></div>';
-            var htmlContent = htmlContent + '<table class="table table-striped table-bordered">';
-            var htmlContent = htmlContent + '<tr><th colspan="11" class="text-center">SECRETARIAT OF THE KERALA LEGISLATURE<br></th></tr>';
-            var htmlContent = htmlContent + '<tr><td>PB No:</td><td>5430</td><td colspan="6"></td><td>GSTN: </td><td><b>32AAAGK0786J1ZD</b></td></tr>';
-            var htmlContent = htmlContent + '<tr><td>PIN:</td><td>695 033</td></tr><tr><td>Email:</td><td>secretary@niyamasabha.nic.in</td></tr>';
-            var htmlContent = htmlContent + '<tr><th class="text-center" colspan="11">INVOICE<br></th></tr>';
-            var htmlContent = htmlContent + '<tr><td>Bill To</td><td><?= $user_prof['org_name']; ?><br><?= $user_prof['head_org_email']; ?></td><td colspan="3"></td><td colspan="3" style="text-align: right;">Invoice No:</td><td colspan="2"><?= $invoice; ?></td></tr>';
-            var htmlContent = htmlContent + '<tr><td>GSTIN:</td><td><?= $user_prof['gst_no']; ?></td><td colspan="3"></td><td colspan="3" style="text-align: right;">Invoice Date:</td><td colspan="2"><?= $chellan['approved_date']; ?></td></tr>';
-            var htmlContent = htmlContent + '<tr><td colspan="11"></td></tr>';
-            var htmlContent = htmlContent + '<tr><td rowspan="2" style="text-align: center;">No</td><td rowspan="2" style="text-align: center;">Item Description</td><td rowspan="2" style="text-align: center;">HSN/SAC</td><td rowspan="2" style="text-align: center;">Qty</td><td rowspan="2" style="text-align: center;">Unit Price</td><td rowspan="2" style="text-align: right;">Taxable Amount</td><td colspan="3" style="text-align: center;">GST</td><td rowspan="2" style="text-align: right;">Total</td></tr>';
-            var htmlContent = htmlContent + '<tr><td style="text-align: center;">%</td><td style="text-align: center;">SGST</td><td style="text-align: center;">CGST</td></tr><tr><td style="text-align: center;">';
+            // AJAX Request
+            $.ajax({
+                url: "generate_pdf.php", // PHP file that generates HTML content
+                type: "POST",
+                data: {
+                    user_id: userId
+                },
+                success: function(response) {
+                    // Set the iframe content dynamically
+                    // const iframe = document.getElementById("print-invoice-frame");
+                    // iframe.style.display = "block";
+                    // const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+                    // iframeDocument.open();
+                    // iframeDocument.write(response);
+                    // iframeDocument.close();
 
-            if (stall3x3 > 0) {
-                var htmlContent = htmlContent + (++slno) + '</td><td>Rent for Stall 3x3m 01/11/2023-07/11/2023</td><td>997222</td><td style="text-align: right;">&emsp;' + stall3x3 + '</td><td style="text-align: right;">10000</td><td style="text-align: right;"><?= $rate3x3; ?></td><td style="text-align: right;">18</td><td style="text-align: right;"><?= ($gst3x3 / 2); ?></td><td style="text-align: right;"><?= ($gst3x3 / 2); ?></td><td style="text-align: right;"><?= $tot_amt3x3; ?>';
-            }
 
-            if (stall3x2 > 0) {
-                var htmlContent = htmlContent + (++slno) + '</td><td>Rent for Stall 3x2m 01/11/2023-07/11/2023</td><td>997222</td><td style="text-align: right;">' + stall3x2 + '</td><td style="text-align: right;">8500</td><td style="text-align: right;"><?= $rate3x2; ?></td><td style="text-align: right;">18</td><td style="text-align: right;"><?= ($gst3x2 / 2); ?></td><td style="text-align: right;"><?= ($gst3x2 / 2); ?></td><td style="text-align: right;"><?= $tot_amt3x2; ?>';
-            }
-            var htmlContent = htmlContent + '</td></tr><tr><td colspan="11"></td></tr><tr><td colspan="3" style="text-align: right;">Total</td><td style="text-align: right;">' + (stall3x3 + stall3x2) + '</td><td></td><td style="text-align: right;"><?= ($rate3x3 + $rate3x2); ?></td><td></td><td style="text-align: right;"><?= (($gst3x3 + $gst3x2) / 2); ?></td><td style="text-align: right;"><?= (($gst3x3 + $gst3x2) / 2); ?></td><td style="text-align: right;"><?= $total_amt; ?></td></tr><tr><td colspan="11"></td></tr><tr><td colspan="4" style="text-align: right;">Total Taxable Amount</td><td colspan="7" style="text-align: right;"><?= ($rate3x3 + $rate3x2); ?></td></tr><tr><td colspan="4" style="text-align: right;">Total Tax Amount</td><td colspan="7" style="text-align: right;"><?= ($gst3x3 + $gst3x2); ?></td></tr><tr><td colspan="4" style="text-align: right;">Total Amount</td><td colspan="7" style="text-align: right;"><?= $total_amt; ?></td></tr><tr><td colspan="4" style="text-align: right;">Amount Due</td><td colspan="7" style="text-align: right;"><?= $total_amt; ?></td></tr><tr><td colspan="4" style="text-align: right;">Total (in words)</td><td colspan="7" style="text-align: right;"><?= $totalinword; ?></td></tr><tr><td colspan="11" style="text-align: right;"><br><br><br>Authorized Signatory</td></tr></table></body></html>';
 
-            var iframe = document.getElementById("print-invoice-frame");
-            iframe.contentDocument.write(htmlContent);
-            iframe.contentDocument.close();
-            iframe.focus(); // Optional: focus on the iframe
-            iframe.contentWindow.print();
+
+
+                    var iframe = document.getElementById("print-invoice-frame");
+                    iframe.contentDocument.write(response);
+                    iframe.contentDocument.close();
+                    iframe.focus(); // Optional: focus on the iframe
+                    iframe.contentWindow.print();
+                },
+                error: function() {
+                    alert("Error while generating the report.");
+                }
+            });
         }
     </script>
---
+++ dashboard/publisher/generate_pdf.php
@@ -0,0 +1,66 @@
+<?php
+include "../z_db.php";
+
+// Fetch data
+$user_id = $_POST['user_id']; // Retrieve user_id from AJAX POST request
+$total_coupon_invoice_query = "SELECT * FROM coupon_publisher_invoice WHERE user_id = '$user_id';";
+$total_bills = mysqli_query($con, $total_coupon_invoice_query);
+
+// Generate HTML content
+$html = '<html><head><title>Coupon Report</title></head><body>';
+$html .= '<h2 style="text-align: center;">KLIBF THIRD EDITION</h2>';
+$html .= '<h3 style="text-align: center;">Book Coupon Details</h3>';
+$html .= '<table border="1" style="width: 100%; border-collapse: collapse; text-align: center;">';
+$html .= '<thead>
+            <tr>
+                <th>Sl No</th>
+                <th>Bill No</th>
+                <th>Bill Date</th>
+                <th>Denomination</th>
+                <th>Count</th>
+                <th>Amount</th>
+                <th>Sub Total</th>
+            </tr>
+          </thead><tbody>';
+
+$counter = 0;
+while ($bill = mysqli_fetch_array($total_bills)) {
+    $coupon200_count = $coupon100_count = $coupon50_count = 0;
+    $invoice_no = $bill['id'];
+
+    $pub_inv_cpn_count_qry = "SELECT COUNT(cd.serial_no) AS serial_no_count, cd.denom_id 
+                              FROM coupon_publisher_serialno cps 
+                              JOIN coupon_distribution cd ON cps.cpn_slno=cd.id 
+                              WHERE cps.cpn_pub_inv = '$invoice_no' GROUP BY cd.denom_id;";
+    $invoice_coupons = mysqli_query($con, $pub_inv_cpn_count_qry);
+    while ($coupon_denom_count = mysqli_fetch_array($invoice_coupons)) {
+        if ($coupon_denom_count['denom_id'] == 1) {
+            $coupon50_count = $coupon_denom_count['serial_no_count'];
+        } elseif ($coupon_denom_count['denom_id'] == 2) {
+            $coupon100_count = $coupon_denom_count['serial_no_count'];
+        } elseif ($coupon_denom_count['denom_id'] == 3) {
+            $coupon200_count = $coupon_denom_count['serial_no_count'];
+        }
+    }
+
+    $total_amount = (50 * $coupon50_count) + (100 * $coupon100_count) + (200 * $coupon200_count);
+
+    // Add row to table
+    // $html .= "<tr>
+    //             <td>" . (++$counter) . "</td>
+    //             <td>" . $bill['invoice_no'] . "</td>
+    //             <td>" . $bill['invoice_dt'] . "</td>
+    //             <td>50, 100, 200</td>
+    //             <td>{$coupon50_count}, {$coupon100_count}, {$coupon200_count}</td>
+    //             <td>{$total_amount}</td>
+    //             <td>{$bill['tot_inv_amt']}</td>
+    //           </tr>";
+
+    $html .= "<tr rowspan="2">
+    
+    
+    </tr>"
+}
+
+$html .= '</tbody></table></body></html>';
+echo $html;
