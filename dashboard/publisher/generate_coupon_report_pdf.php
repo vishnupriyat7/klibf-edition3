00499f9373a9cffa9594758e7c40c932361b6d01
parvathimpillai@gmail.com <parvathimpillai@gmail.com>
2025-01-15 15:07:16 +0530

    generate pdf with date dne

--- dashboard/publisher/generate_coupon_report_pdf.php
+++ dashboard/publisher/generate_coupon_report_pdf.php
@@ -95,22 +95,15 @@ function convertNumberToWordsForIndia($number)
 // Fetch data
 $user_id = $_POST['user_id']; // Retrieve user_id from AJAX POST request
 $updated_date = $_POST['date'];
-$total_coupon_invoice_query = "SELECT * FROM coupon_publisher_invoice WHERE user_id = '$user_id';";
+$total_coupon_invoice_query = "SELECT * FROM coupon_publisher_invoice WHERE user_id = '$user_id' AND updated_date >= '$updated_date';";
 $total_bills = mysqli_query($con, $total_coupon_invoice_query);
-
-
 $publisher_dtls_query = "SELECT org_name, head_org_mobile FROM users_profile WHERE user_id = '$user_id';";
 $publisher_dtls = mysqli_query($con, $publisher_dtls_query);
 $publisher_dtls_row = mysqli_fetch_array($publisher_dtls);
-
-
-
 // Generate HTML content
-
 $html = '<html><head><title>Summary</title></head><body><br>';
 $html .= '<p style="text-align: left;"><strong>Publisher Name :  ' . $publisher_dtls_row['org_name'] . '</strong> <p>';
 $html .= '<p style="text-align: left;"><strong>Contact Number:  ' . $publisher_dtls_row['head_org_mobile'] . '</strong> <p><br>';
-
 $html .= '<table border="1" style="width: 100%; border-collapse: collapse; text-align: center;">';
 $html .= '<thead>
             <tr>
@@ -124,136 +117,105 @@ $html .= '<thead>
                 <th>Sub Total</th>
                         </tr>
           </thead><tbody>';
-
 $counter = 0;
 $grand_total = 0;
 $coupon50_total_count = 0;
 $coupon100_total_count = 0;
 $coupon200_total_count = 0;
-
-while ($bill = mysqli_fetch_array($total_bills)) {
-    $coupon200_count = $coupon100_count = $coupon50_count = 0;
-    $invoice_no = $bill['id'];
-
-    $pub_inv_cpn_count_qry = "SELECT COUNT(cd.serial_no) AS serial_no_count, cd.denom_id 
+if ($total_bills->num_rows > 0) {
+    while ($bill = mysqli_fetch_array($total_bills)) {
+        $coupon200_count = $coupon100_count = $coupon50_count = 0;
+        $invoice_no = $bill['id'];
+        $pub_inv_cpn_count_qry = "SELECT COUNT(cd.serial_no) AS serial_no_count, cd.denom_id 
                               FROM coupon_publisher_serialno cps 
                               JOIN coupon_distribution cd ON cps.cpn_slno=cd.id 
-                              JOIN coupon_publisher_invoice cpi ON cps.cpn_pub_inv = cpi.id
-                              WHERE cps.cpn_pub_inv = '$invoice_no' AND cpi.updated_date >= '$updated_date' GROUP BY cd.denom_id;";
-    $invoice_coupons = mysqli_query($con, $pub_inv_cpn_count_qry);
-    while ($coupon_denom_count = mysqli_fetch_array($invoice_coupons)) {
-        if ($coupon_denom_count['denom_id'] == 1) {
-            $coupon50_count = $coupon_denom_count['serial_no_count'];
-        } elseif ($coupon_denom_count['denom_id'] == 2) {
-            $coupon100_count = $coupon_denom_count['serial_no_count'];
-        } elseif ($coupon_denom_count['denom_id'] == 3) {
-            $coupon200_count = $coupon_denom_count['serial_no_count'];
+                              WHERE cps.cpn_pub_inv = '$invoice_no' GROUP BY cd.denom_id;";
+        $invoice_coupons = mysqli_query($con, $pub_inv_cpn_count_qry);
+        while ($coupon_denom_count = mysqli_fetch_array($invoice_coupons)) {
+            if ($coupon_denom_count['denom_id'] == 1) {
+                $coupon50_count = $coupon_denom_count['serial_no_count'];
+            } elseif ($coupon_denom_count['denom_id'] == 2) {
+                $coupon100_count = $coupon_denom_count['serial_no_count'];
+            } elseif ($coupon_denom_count['denom_id'] == 3) {
+                $coupon200_count = $coupon_denom_count['serial_no_count'];
+            }
         }
-    }
-
-    // $total_amount = (50 * $coupon50_count) + (100 * $coupon100_count) + (200 * $coupon200_count);
-    $sub_total = (50 * $coupon50_count) + (100 * $coupon100_count) + (200 * $coupon200_count);
-    $grand_total = (int) $grand_total + (int) $sub_total;
-
-    $coupon50_total_count = $coupon50_count + $coupon50_total_count;
-    $coupon100_total_count = $coupon100_count + $coupon100_total_count;
-    $coupon200_total_count = $coupon200_count + $coupon200_total_count;
-
-
-    $coupon50_total_amount = $coupon50_total_count * 50;
-    $coupon100_total_amount = $coupon100_total_count * 100;
-    $coupon200_total_amount = $coupon200_total_count * 200;
-    // $sub_total = (50 * $coupon50_count) + (100 * $coupon100_count) + (200 * $coupon200_count);
-
-    // $total_amount =  $coupon50_total_amount +  $coupon100_total_amount +  $coupon200_total_amount;
-
-    // Add row to table
-    // $html .= "<tr>
-    //             <td>" . (++$counter) . "</td>
-    //             <td>" . $bill['invoice_no'] . "</td>
-    //             <td>" . $bill['invoice_dt'] . "</td>
-    //             <td>50, 100, 200</td>
-    //             <td>{$coupon50_count}, {$coupon100_count}, {$coupon200_count}</td>
-    //             <td>{$total_amount}</td>
-    //             <td>{$bill['tot_inv_amt']}</td>
-    //           </tr>";
-    $html .= "<tr>
-                <td rowspan='3'>" . (++$counter) . "</td>
-               
+        $sub_total = (50 * $coupon50_count) + (100 * $coupon100_count) + (200 * $coupon200_count);
+        $grand_total = (int) $grand_total + (int) $sub_total;
+        $coupon50_total_count = $coupon50_count + $coupon50_total_count;
+        $coupon100_total_count = $coupon100_count + $coupon100_total_count;
+        $coupon200_total_count = $coupon200_count + $coupon200_total_count;
+        $coupon50_total_amount = $coupon50_total_count * 50;
+        $coupon100_total_amount = $coupon100_total_count * 100;
+        $coupon200_total_amount = $coupon200_total_count * 200;
+        $html .= "<tr>
+                <td rowspan='3'>" . (++$counter) . "</td>               
                 <td rowspan='3'>{$bill['invoice_no']} & " . date('d-m-Y', strtotime($bill['invoice_dt'])) . "</td>
                 <td>50</td>
                 <td>{$coupon50_count}</td>
                 <td>" . (50 * $coupon50_count) . "</td>
                 <td rowspan='3'><strong>" . $sub_total . "</strong></td>
               </tr>";
-    $html .= "<tr>
+        $html .= "<tr>
                 <td>100</td>
                 <td>{$coupon100_count}</td>
                 <td>" . (100 * $coupon100_count) . "</td>
               </tr>";
-    $html .= "<tr>
+        $html .= "<tr>
                 <td>200</td>
                 <td>{$coupon200_count}</td>
                 <td>" . (200 * $coupon200_count) . "</td>
                
                              </tr>";
-}
-// var_dump($grand_total);
-$html .= "<tr>
+    }
+    $html .= "<tr>
 <td colspan='5'><strong>Grand Total</strong></td>
 <td><strong>" . $grand_total . "</strong></td></tr>";
-
-$html .= '</tbody></table><br><br>';
-$html .= '<table border="1" style="width: 100%; border-collapse: collapse; text-align: center;">';
-$html .= '<thead>
+    $html .= '</tbody></table><br><br>';
+    $html .= '<table border="1" style="width: 100%; border-collapse: collapse; text-align: center;">';
+    $html .= '<thead>
 <tr><th colspan="4">Bank Details</th></tr>
-            <tr>
-                
+            <tr>                
                 <th>Bank Name</th>
                 <th>Branch</th>
                 <th>IFSC</th>
-                <th>Account Number</th>
-                            
+                <th>Account Number</th>                            
             </tr>
           </thead><tbody>';
-$cpn_bnk_dtls_qry = "SELECT * FROM pub_coupon_bankdtls WHERE user_id = $user_id;";
-$result_bnk_dtls = mysqli_query($con, $cpn_bnk_dtls_qry);
-$cpn_bnk_dtls = $result_bnk_dtls->fetch_assoc();
-// var_dump($cpn_bnk_dtls);
+    $cpn_bnk_dtls_qry = "SELECT * FROM pub_coupon_bankdtls WHERE user_id = $user_id;";
+    $result_bnk_dtls = mysqli_query($con, $cpn_bnk_dtls_qry);
+    $cpn_bnk_dtls = $result_bnk_dtls->fetch_assoc();
 
-$html .= '<tr>
+    $html .= '<tr>
           <td>' . ($cpn_bnk_dtls["bank_name"]) . '</td>
             <td>' . ($cpn_bnk_dtls["bank_branch"]) . '</td>
               <td>' . ($cpn_bnk_dtls["bank_ifsc"]) . '</td>
                 <td>' . ($cpn_bnk_dtls["account_no"]) . '</td>
           </tr> ';
-$html .= '</tbody></table><br><br>';
-
-
-$grandtotal_words = convertNumberToWordsForIndia($grand_total);
-$html .= '<p>I here by submitting Coupons worth <strong>' . $grand_total . '/- </strong> (Grand Total)  <strong>' . ($grandtotal_words) . '</strong> only in the below mentioned denominations. </p>';
+    $html .= '</tbody></table><br><br>';
+    $grandtotal_words = convertNumberToWordsForIndia($grand_total);
+    $html .= '<p>I here by submitting Coupons worth <strong>' . $grand_total . '/- </strong> (Grand Total)  <strong>' . ($grandtotal_words) . '</strong> only in the below mentioned denominations. </p>';
+} else {
+    $html .= "<tr><td colspan='6'>No data</td></tr>";
+}
 $html .= '</tbody></table><br><br><br>';
-
-
-
 $html .= '<table border="0" style="width: 100%; text-align: center;">';
 $html .= '<thead></thead>
             <tbody>
-
             <tr>
-                                <td>50</td>
+                 <td>50</td>
                 <td>' . $coupon50_total_count . '</td>
                 <td>' . $coupon50_total_amount . '</td>
                                            
             </tr>
             <tr>
-                                <td>100</td>
+                <td>100</td>
                 <td>' . $coupon100_total_count . '</td>
                 <td>' . $coupon100_total_amount . '</td>
                                            
             </tr>
             <tr>
-                                <td>200</td>
+                <td>200</td>
                 <td>' . $coupon200_total_count . '</td>
                 <td>' . $coupon200_total_amount . '</td>
                                            
@@ -269,7 +231,5 @@ $html .= '<table style="width: 100%; border: none; margin-top: 20px;">
                 <td style="text-align: right; width: 50%;">Authorised Signatory <br>(Seal)</td>
             </tr>
           </table>';
-
-
 $html .= '</body></html>';
 echo $html;
